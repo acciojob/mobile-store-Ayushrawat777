0c8e955683740ff430544eaa42ccaf25dcf023ca
AYUSH RAWAT <106034287+Ayushrawat777@users.noreply.github.com>
2025-03-04 00:42:08 +0530

    Add files via upload

--- src/components/App.js
+++ src/components/App.js
@@ -1,104 +1,31 @@
-import React from 'react';
-
-import {
-  BrowserRouter,
-  Route,
-  Routes,
-  useParams,
-  Link,
-  NavLink,
-} from "react-router-dom";
-
-const products = [
-  { id: 1, name: "Mobile 1", description: "Description 1", price: "$200" },
-  { id: 2, name: "Mobile 2", description: "Description 2", price: "$300" },
-  { id: 3, name: "Mobile 3", description: "Description 3", price: "$400" },
-  { id: 4, name: "Mobile 4", description: "Description 4", price: "$400" },
-  { id: 5, name: "Mobile 5", description: "Description 5", price: "$400" },
-  { id: 6, name: "Mobile 6", description: "Description 6", price: "$400" },
-];
-
-function AdminProdcut() {
-  const { id } = useParams();
+ import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ProductList from "./ProductList";
+import ProductDetails from "./ProductDetails";
+import AdminPanel from "./AdminPanel";
+import { initialProducts } from "./Data";
+import "../App.css";
+import Navbar from "./Navbar";
+const App = () => {
+  const [products, setProducts] = useState(initialProducts);
 
   return (
-    <div>
-      <p>Product {id}</p>
-      <input name="title" id="title" />
-
-      <button>Delete</button>
-      <button>Save</button>
-    </div>
-  );
-}
-
-function Home() {
-  return (
-    <>
-      <PageNav />
-      <div className="col-12">
-        <div>
-          {products.map((item) => (
-            <Link key={item.id} to={`/products/${item.id}`}>
-              {item.name} <button>Buy</button>
-            </Link>
-          ))}
-        </div>
-      </div>
-    </>
-  );
-}
-
-function Product() {
-  const { id } = useParams();
-
-  return (
-    <>
-      <PageNav />
-      <div>
-        <h1>Product {id} </h1>
-        <Link to="/">
-          <button className="btn">Other Products</button>
-        </Link>
-      </div>
-    </>
-  );
-}
-
-function Admin() {
-  return (
-    <>
-      <PageNav />
-      <h1>Admin Panel</h1>
-      {products.map((item) => (
-          <NavLink key={item.id} to={`products/${item.id}`}>
-            {item.name}
-          </NavLink>
-      ))}
-    </>
-  );
-}
-
-function PageNav() {
-  return (
-    <>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/admin">Admin</NavLink>
-    </>
-  );
-}
-
-function App() {
-  return (
-    <BrowserRouter>
+    <Router>
+      <Navbar/>
       <Routes>
-        <Route index element={<Home />} />
-        <Route path="admin" element={<Admin />} />
-        <Route path="products/:id" element={<Product />} />
-        <Route path="admin/products/:id" element={<AdminProdcut />} />
+        <Route path="/" element={<ProductList products={products} />} />
+        <Route
+          path="/products/:id"
+          element={<ProductDetails products={products} />}
+        />
+        <Route
+          path="/admin"
+          element={<AdminPanel products={products} setProducts={setProducts} />}
+        />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
-}
+};
 
 export default App;
+ 
